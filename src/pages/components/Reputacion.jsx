d13370f0736d8b0e8f6565096e89b228f83cf0fa
krosslerfrancisco <krosslerfrancisco@gmail.com>
2024-11-01 13:19:49 +0000

    commit

--- src/pages/components/Reputacion.jsx
+++ src/pages/components/Reputacion.jsx
@@ -4,9 +4,9 @@ const Reputacion = () => {
     return(
         <section className="reputacion">
             <h1>Confianza y Calidad</h1>
-            <div className="reputacion-icons">
             <p>Somos <span>Mercado Líder Platinum</span>, garantizamos la máxima confianza en cada compra.</p>
             <p>Con nuestros envíos <span>FULL</span> y <span>Flex</span>, tus productos llegan a tiempo, asegurando una experiencia de compra rápida y eficiente.</p>
+            <div className="reputacion-icons">
                 <figure>
                     <div className="img-container">
                         <img width={120} src="./full.png" alt="mercado lider platinum" />
