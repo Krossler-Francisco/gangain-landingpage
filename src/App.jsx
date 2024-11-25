fdc066a1291b5921ab481d8932fc6975880a887c
krosslerfrancisco <krosslerfrancisco@gmail.com>
2024-11-25 21:21:47 +0000

    page deploy test

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Destaques from './pages/Destaques.jsx';
 import Gallery from './pages/Gallery.jsx';
 import Header from './pages/Header.jsx';
